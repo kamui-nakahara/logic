62d0413218a87a6e658cb946db2ccba83e1ecdb4
kamui-nakahara <guiafc1266310378@gmail.com>
2023-07-21 14:22:23 +0900

    second commit

--- src/Display.java
+++ src/Display.java
@@ -13,10 +13,12 @@ import java.awt.event.KeyEvent;
 import java.awt.Graphics;
 import java.awt.Point;
 import java.awt.Color;
+import java.util.ArrayList;
 
 class Display extends JFrame implements KeyListener{
   public int width;
   public int height;
+  Screen screen;
   Display(String title,int width,int height){
     super(title);
     this.width=width;
@@ -41,119 +43,119 @@ class Display extends JFrame implements KeyListener{
     JMenuItem item3_1=new JMenuItem("2入力ANDゲート");
     item3_1.addActionListener(new ActionListener(){
       public void actionPerformed(ActionEvent e){
-	Gate.having="and2";
+	screen.gate=new And(screen.mousePoint,2);
       }
     });
     menu3.add(item3_1);
     JMenuItem item3_2=new JMenuItem("3入力ANDゲート");
     item3_2.addActionListener(new ActionListener(){
       public void actionPerformed(ActionEvent e){
-	Gate.having="and3";
+	screen.gate=new And(screen.mousePoint,3);
       }
     });
     menu3.add(item3_2);
     JMenuItem item3_3=new JMenuItem("2入力ORゲート");
     item3_3.addActionListener(new ActionListener(){
       public void actionPerformed(ActionEvent e){
-	Gate.having="or2";
+	screen.gate=new Or(screen.mousePoint,2);
       }
     });
     menu3.add(item3_3);
     JMenuItem item3_4=new JMenuItem("3入力ORゲート");
     item3_4.addActionListener(new ActionListener(){
       public void actionPerformed(ActionEvent e){
-	Gate.having="or3";
+	screen.gate=new Or(screen.mousePoint,3);
       }
     });
     menu3.add(item3_4);
     JMenuItem item3_5=new JMenuItem("NOTゲート");
     item3_5.addActionListener(new ActionListener(){
       public void actionPerformed(ActionEvent e){
-	Gate.having="not";
+	screen.gate=new Not(screen.mousePoint);
       }
     });
     menu3.add(item3_5);
     JMenuItem item3_6=new JMenuItem("2入力NANDゲート");
     item3_6.addActionListener(new ActionListener(){
       public void actionPerformed(ActionEvent e){
-	Gate.having="nand2";
+	screen.gate=new Nand(screen.mousePoint,2);
       }
     });
     menu3.add(item3_6);
     JMenuItem item3_7=new JMenuItem("3入力NANDゲート");
     item3_7.addActionListener(new ActionListener(){
       public void actionPerformed(ActionEvent e){
-	Gate.having="nand3";
+	screen.gate=new Nand(screen.mousePoint,3);
       }
     });
     menu3.add(item3_7);
     JMenuItem item3_8=new JMenuItem("2入力NORゲート");
     item3_8.addActionListener(new ActionListener(){
       public void actionPerformed(ActionEvent e){
-	Gate.having="nor2";
+	screen.gate=new Nor(screen.mousePoint,2);
       }
     });
     menu3.add(item3_8);
     JMenuItem item3_9=new JMenuItem("3入力NORゲート");
     item3_9.addActionListener(new ActionListener(){
       public void actionPerformed(ActionEvent e){
-	Gate.having="nor3";
+	screen.gate=new Nor(screen.mousePoint,3);
       }
     });
     menu3.add(item3_9);
     JMenuItem item3_10=new JMenuItem("2入力XORゲート");
     item3_10.addActionListener(new ActionListener(){
       public void actionPerformed(ActionEvent e){
-	Gate.having="xor2";
+	screen.gate=new Xor(screen.mousePoint,2);
       }
     });
     menu3.add(item3_10);
     JMenuItem item3_11=new JMenuItem("3入力XORゲート");
     item3_11.addActionListener(new ActionListener(){
       public void actionPerformed(ActionEvent e){
-	Gate.having="xor3";
+	screen.gate=new Xor(screen.mousePoint,3);
       }
     });
     menu3.add(item3_11);
     JMenuItem item3_12=new JMenuItem("2入力XNORゲート");
     item3_12.addActionListener(new ActionListener(){
       public void actionPerformed(ActionEvent e){
-	Gate.having="xnor2";
+	screen.gate=new Xnor(screen.mousePoint,2);
       }
     });
     menu3.add(item3_12);
     JMenuItem item3_13=new JMenuItem("3入力XNORゲート");
     item3_13.addActionListener(new ActionListener(){
       public void actionPerformed(ActionEvent e){
-	Gate.having="xnor3";
+	screen.gate=new Xnor(screen.mousePoint,3);
       }
     });
     menu3.add(item3_13);
     JMenuItem item3_14=new JMenuItem("入力ノード");
     item3_14.addActionListener(new ActionListener(){
       public void actionPerformed(ActionEvent e){
-	Gate.having="input";
+	screen.gate=new Input(screen.mousePoint,false);
       }
     });
     menu3.add(item3_14);
     JMenuItem item3_15=new JMenuItem("出力ノード");
     item3_15.addActionListener(new ActionListener(){
       public void actionPerformed(ActionEvent e){
-	Gate.having="output";
+	screen.gate=new Output(screen.mousePoint,false);
       }
     });
     menu3.add(item3_15);
     JMenuItem item3_16=new JMenuItem("結線");
     item3_16.addActionListener(new ActionListener(){
       public void actionPerformed(ActionEvent e){
-	Gate.having="line";
+	screen.gate=new Line(screen.mousePoint);
       }
     });
     menu3.add(item3_16);
     menubar.add(menu3);
     setJMenuBar(menubar);
 
-    Screen screen=new Screen(this);
+    this.screen=new Screen(this);
     add(screen);
 
     addKeyListener(this);
@@ -173,19 +175,30 @@ class Display extends JFrame implements KeyListener{
   @Override
   public void keyReleased(KeyEvent e){
   }
+  public void update(){
+    screen.update();
+  }
 }
 
 class Screen extends JPanel implements MouseMotionListener,MouseListener{
-  Point mousePoint=new Point(0,0);
+  public Point mousePoint=new Point(0,0);
   Display display;
   public static int interval=10;
   int state=0; //0:ゲート設置 1:削除
   Color mouseColor=new Color(128,128,128,128);
+  public Gate gate=new And(mousePoint,2);
+  ArrayList<Gate> gates=new ArrayList<>();
   Screen(Display display){
     super();
     this.display=display;
     addMouseMotionListener(this);
     addMouseListener(this);
+  }
+  public void update(){
+    gate.update();
+    for (Gate gate:gates){
+      gate.update();
+    }
   }
   public void paintComponent(Graphics g){
     super.paintComponent(g);
@@ -195,54 +208,11 @@ class Screen extends JPanel implements MouseMotionListener,MouseListener{
     switch(state){
       case 0:
 	g.setColor(Color.BLACK);
-	switch(Gate.having){
-	  case "and2":
-	    Gate.and2(g,mousePoint.x,mousePoint.y);
-	    break;
-	  case "and3":
-	    Gate.and3(g,mousePoint.x,mousePoint.y);
-	    break;
-	  case "or2":
-	    Gate.or2(g,mousePoint.x,mousePoint.y);
-	    break;
-	  case "or3":
-	    Gate.or3(g,mousePoint.x,mousePoint.y);
-	    break;
-	  case "not":
-	    Gate.not(g,mousePoint.x,mousePoint.y);
-	    break;
-	  case "nand2":
-	    Gate.nand2(g,mousePoint.x,mousePoint.y);
-	    break;
-	  case "nand3":
-	    Gate.nand3(g,mousePoint.x,mousePoint.y);
-	    break;
-	  case "nor2":
-	    Gate.nor2(g,mousePoint.x,mousePoint.y);
-	    break;
-	  case "nor3":
-	    Gate.nor3(g,mousePoint.x,mousePoint.y);
-	    break;
-	  case "xor2":
-	    Gate.xor2(g,mousePoint.x,mousePoint.y);
-	    break;
-	  case "xor3":
-	    Gate.xor3(g,mousePoint.x,mousePoint.y);
-	    break;
-	  case "xnor2":
-	    Gate.xnor2(g,mousePoint.x,mousePoint.y);
-	    break;
-	  case "xnor3":
-	    Gate.xnor3(g,mousePoint.x,mousePoint.y);
-	    break;
-	  case "input":
-	    Gate.input(g,mousePoint.x,mousePoint.y,false);
-	    break;
-	  case "output":
-	    Gate.output(g,mousePoint.x,mousePoint.y,false);
-	    break;
-	}
+	gate.draw(g);
 	break;
+    }
+    for (Gate gate:gates){
+      gate.draw(g);
     }
   }
   void background(Graphics g){
@@ -263,6 +233,50 @@ class Screen extends JPanel implements MouseMotionListener,MouseListener{
   public void mouseMoved(MouseEvent e){
     mousePoint.x=(int)((e.getPoint().x+interval/2)/interval)*interval;
     mousePoint.y=(int)((e.getPoint().y+interval/2)/interval)*interval;
+    gate.setPos(mousePoint);
+    gate.over=new ArrayList<>();
+    System.out.print("input1:");
+    for (Gate g:gates){
+      for (Point input:g.inputs){
+	System.out.print(input+",");
+      }
+    }
+    System.out.println("");
+    System.out.print("input2:");
+    for (Point input:gate.inputs){
+      System.out.print(input+",");
+    }
+    System.out.println("");
+    System.out.print("output1:");
+    for (Gate g:gates){
+      for (Point output:g.outputs){
+	System.out.print(output+",");
+      }
+    }
+    System.out.println("");
+    System.out.print("output2:");
+    for (Point output:gate.outputs){
+      System.out.print(output+",");
+    }
+    System.out.println("");
+    for (Gate g:gates){
+      for (Point input:gate.inputs){
+	for (Point output:g.outputs){
+	  if (output.equals(input)){
+	    gate.over.add(input);
+	    break;
+	  }
+	}
+      }
+      for (Point output:gate.outputs){
+	for (Point input:g.inputs){
+	  if (input.equals(output)){
+	    gate.over.add(output);
+	    break;
+	  }
+	}
+      }
+    }
   }
   @Override
   public void mouseExited(MouseEvent e){
@@ -279,6 +293,48 @@ class Screen extends JPanel implements MouseMotionListener,MouseListener{
   }
   @Override
   public void mouseReleased(MouseEvent e){
+    mousePoint.x=(int)((e.getPoint().x+interval/2)/interval)*interval;
+    mousePoint.y=(int)((e.getPoint().y+interval/2)/interval)*interval;
     mouseColor=new Color(128,128,128,128);
+    if (gate.name=="line"){
+      if (gate.getBegin()==null){
+	gate.setBegin(new Point(mousePoint.x,mousePoint.y));
+      }else if (gate.getEnd()==null){
+	gate.setEnd(new Point(mousePoint.x,mousePoint.y));
+	if (!gate.getBegin().equals(gate.getEnd()))gates.add(gate);
+	gate=new Line(new Point(gate.X,gate.Y));
+      }
+    }else{
+      gates.add(gate);
+      switch(gate.name){
+	case "and":
+	  gate=new And(new Point(gate.X,gate.Y),gate.terminals);
+	  break;
+	case "or":
+	  gate=new Or(new Point(gate.X,gate.Y),gate.terminals);
+	  break;
+	case "not":
+	  gate=new Not(new Point(gate.X,gate.Y));
+	  break;
+	case "nand":
+	  gate=new Nand(new Point(gate.X,gate.Y),gate.terminals);
+	  break;
+	case "nor":
+	  gate=new Nor(new Point(gate.X,gate.Y),gate.terminals);
+	  break;
+	case "xor":
+	  gate=new Xor(new Point(gate.X,gate.Y),gate.terminals);
+	  break;
+	case "xnor":
+	  gate=new Xnor(new Point(gate.X,gate.Y),gate.terminals);
+	  break;
+	case "input":
+	  gate=new Input(new Point(gate.X,gate.Y),gate.truthValue);
+	  break;
+	case "output":
+	  gate=new Output(new Point(gate.X,gate.Y),gate.truthValue);
+	  break;
+      }
+    }
   }
 }
--- src/Gate.java
+++ src/Gate.java
@@ -2,14 +2,90 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.geom.Arc2D;
 import java.awt.Color;
+import java.awt.Point;
 import java.awt.geom.Path2D;
+import java.util.ArrayList;
 
 class Gate{
-  public static String having="and2";
-  public static void and2(Graphics g,int x,int y){
+  public String name;
+  public int X;
+  public int Y;
+  ArrayList<Point> inputs_e=new ArrayList<>();//入力端子の相対座標
+  ArrayList<Point> outputs_e=new ArrayList<>();//出力端子の相対座標
+  public ArrayList<Point> inputs=new ArrayList<>();//入力端子の絶対座標
+  public ArrayList<Point> outputs=new ArrayList<>();//出力端子の絶対座標
+  public Point begin;
+  public Point end;
+  public boolean truthValue;
+  public int terminals;
+  ArrayList<Point> over=new ArrayList<>();//重なってる端子を赤色にする
+  Gate(Point point){
+    this.X=point.x;
+    this.Y=point.y;
+  }
+  public void update(){
+    inputs=new ArrayList<>();
+    outputs=new ArrayList<>();
+    int x=X-Screen.interval*2;
+    int y=Y-Screen.interval*2;
+    for (Point p:inputs_e){
+      inputs.add(new Point(p.x+x,p.y+y));
+    }
+    for (Point p:outputs_e){
+      outputs.add(new Point(p.x+x,p.y+y));
+    }
+  }
+  public void draw(Graphics g){
+  }
+  public void setBegin(Point point){
+  }
+  public void setEnd(Point point){
+  }
+  public Point getBegin(){
+    return null;
+  }
+  public Point getEnd(){
+    return null;
+  }
+  public void setPos(Point point){
+    this.X=point.x;
+    this.Y=point.y;
+  }
+  public void Over(){
+  }
+  void dot(Graphics g,int x,int y){
     Graphics2D g2=(Graphics2D)g;
-    x-=Screen.interval*2;
-    y-=Screen.interval*2;
+    boolean flag=false;
+    for (Point point:over){
+      if (point.equals(new Point(x,y))){
+	flag=true;
+	break;
+      }
+    }
+    g.setColor((flag) ? Color.RED : Color.WHITE);
+    g.fillOval(x-4,y-4,8,8);
+    g.setColor(Color.BLACK);
+    g.drawOval(x-4,y-4,8,8);
+  }
+}
+
+class And extends Gate{
+  public int terminals;
+  And(Point point,int terminals){
+    super(point);
+    this.terminals=terminals;
+    inputs_e.add(new Point(-20,10));
+    inputs_e.add(new Point(-20,30));
+    outputs_e.add(new Point(70,20));
+    if (terminals==3){
+      inputs_e.add(new Point(-20,20));
+    }
+    this.name="and";
+  }
+  public void draw(Graphics g){
+    Graphics2D g2=(Graphics2D)g;
+    int x=X-Screen.interval*2;
+    int y=Y-Screen.interval*2;
     g.drawLine(x+30,y,x,y);
     g.drawLine(x,y,x,y+40);
     g.drawLine(x,y+40,x+30,y+40);
@@ -22,30 +98,30 @@ class Gate{
     dot(g,x-20,y+30);
     dot(g,x+70,y+20);
 
-    //return
+    if (terminals==3){
+      g.drawLine(x,y+20,x-16,y+20);
+      dot(g,x-20,y+20);
+    }
   }
-  public static void and3(Graphics g,int x,int y){
-    Graphics2D g2=(Graphics2D)g;
-    x-=Screen.interval*2;
-    y-=Screen.interval*2;
-    g.drawLine(x+30,y,x,y);
-    g.drawLine(x,y,x,y+40);
-    g.drawLine(x,y+40,x+30,y+40);
-    g.drawLine(x+50,y+20,x+66,y+20);
-    g.drawLine(x,y+10,x-16,y+10);
-    g.drawLine(x,y+30,x-16,y+30);
-    g.drawLine(x,y+20,x-16,y+20);
-    g2.draw(new Arc2D.Double(x+10,y,40,40,-90,180,Arc2D.OPEN));
+}
 
-    dot(g,x-20,y+10);
-    dot(g,x-20,y+20);
-    dot(g,x-20,y+30);
-    dot(g,x+70,y+20);
+class Or extends Gate{
+  public int terminals;
+  Or(Point point,int terminals){
+    super(point);
+    this.terminals=terminals;
+    inputs_e.add(new Point(-20,10));
+    inputs_e.add(new Point(-20,30));
+    outputs_e.add(new Point(70,20));
+    if (terminals==3){
+      inputs_e.add(new Point(-20,20));
+    }
+    this.name="or";
   }
-  public static void or2(Graphics g,int x,int y){
+  public void draw(Graphics g){
     Graphics2D g2=(Graphics2D)g;
-    x-=Screen.interval*2;
-    y-=Screen.interval*2;
+    int x=X-Screen.interval*2;
+    int y=Y-Screen.interval*2;
     g.drawLine(x,y,x+20,y);
     g.drawLine(x,y+40,x+20,y+40);
     g.drawLine(x+55,y+20,x+66,y+20);
@@ -58,58 +134,53 @@ class Gate{
     dot(g,x-20,y+10);
     dot(g,x-20,y+30);
     dot(g,x+70,y+20);
-  }
-  public static void or3(Graphics g,int x,int y){
-    Graphics2D g2=(Graphics2D)g;
-    x-=Screen.interval*2;
-    y-=Screen.interval*2;
-    g.drawLine(x,y,x+20,y);
-    g.drawLine(x,y+40,x+20,y+40);
-    g.drawLine(x+55,y+20,x+66,y+20);
-    g.drawLine(x+3,y+10,x-16,y+10);
-    g.drawLine(x+3,y+30,x-16,y+30);
-    g.drawLine(x+3,y+20,x-16,y+20);
-    g2.draw(new Arc2D.Double(x-75,y-20,80,80,-30,60,Arc2D.OPEN));
-    g2.draw(new Arc2D.Double(x-20,y,80,80,90,-60,Arc2D.OPEN));
-    g2.draw(new Arc2D.Double(x-20,y-40,80,80,-90,60,Arc2D.OPEN));
 
-    dot(g,x-20,y+10);
-    dot(g,x-20,y+20);
-    dot(g,x-20,y+30);
-    dot(g,x+70,y+20);
+    if (terminals==3){
+      g.drawLine(x,y+20,x-16,y+20);
+      dot(g,x-20,y+20);
+    }
   }
-  public static void nand2(Graphics g,int x,int y){
+}
+
+class Nand extends And{
+  Nand(Point point,int terminals){
+    super(point,terminals);
+    this.name="nand";
+  }
+  public void draw(Graphics g){
+    super.draw(g);
     Graphics2D g2=(Graphics2D)g;
-    and2(g,x,y);
-    x-=Screen.interval*2;
-    y-=Screen.interval*2;
+    int x=X-Screen.interval*2;
+    int y=Y-Screen.interval*2;
     dot(g,x+54,y+20);
   }
-  public static void nand3(Graphics g,int x,int y){
-    Graphics2D g2=(Graphics2D)g;
-    and3(g,x,y);
-    x-=Screen.interval*2;
-    y-=Screen.interval*2;
-    dot(g,x+54,y+20);
+}
+
+class Nor extends Or{
+  Nor(Point point,int terminals){
+    super(point,terminals);
+    this.name="nor";
   }
-  public static void nor2(Graphics g,int x,int y){
+  public void draw(Graphics g){
+    super.draw(g);
     Graphics2D g2=(Graphics2D)g;
-    or2(g,x,y);
-    x-=Screen.interval*2;
-    y-=Screen.interval*2;
+    int x=X-Screen.interval*2;
+    int y=Y-Screen.interval*2;
     dot(g,x+57,y+20);
   }
-  public static void nor3(Graphics g,int x,int y){
-    Graphics2D g2=(Graphics2D)g;
-    or3(g,x,y);
-    x-=Screen.interval*2;
-    y-=Screen.interval*2;
-    dot(g,x+57,y+20);
+}
+
+class Not extends Gate{
+  Not(Point point){
+    super(point);
+    inputs_e.add(new Point(-20,20));
+    outputs_e.add(new Point(70,20));
+    this.name="not";
   }
-  public static void not(Graphics g,int x,int y){
+  public void draw(Graphics g){
     Graphics2D g2=(Graphics2D)g;
-    x-=Screen.interval*2;
-    y-=Screen.interval*2;
+    int x=X-Screen.interval*2;
+    int y=Y-Screen.interval*2;
     g.drawLine(x,y,x,y+40);
     g.drawLine(x,y+40,x+28,y+20);
     g.drawLine(x+28,y+20,x,y);
@@ -120,42 +191,47 @@ class Gate{
     dot(g,x-20,y+20);
     dot(g,x+70,y+20);
   }
-  public static void xor2(Graphics g,int x,int y){
+}
+
+class Xor extends Or{
+  Xor(Point point,int terminals){
+    super(point,terminals);
+    this.name="xor";
+  }
+  public void draw(Graphics g){
+    super.draw(g);
     Graphics2D g2=(Graphics2D)g;
-    or2(g,x,y);
-    x-=Screen.interval*2;
-    y-=Screen.interval*2;
+    int x=X-Screen.interval*2;
+    int y=Y-Screen.interval*2;
     g2.draw(new Arc2D.Double(x-80,y-20,80,80,-30,60,Arc2D.OPEN));
   }
-  public static void xor3(Graphics g,int x,int y){
+}
+
+class Xnor extends Nor{
+  Xnor(Point point,int terminals){
+    super(point,terminals);
+    this.name="xnor";
+  }
+  public void draw(Graphics g){
+    super.draw(g);
     Graphics2D g2=(Graphics2D)g;
-    or3(g,x,y);
-    x-=Screen.interval*2;
-    y-=Screen.interval*2;
+    int x=X-Screen.interval*2;
+    int y=Y-Screen.interval*2;
     g2.draw(new Arc2D.Double(x-80,y-20,80,80,-30,60,Arc2D.OPEN));
   }
-  public static void xnor2(Graphics g,int x,int y){
-    Graphics2D g2=(Graphics2D)g;
-    x-=Screen.interval*2;
-    y-=Screen.interval*2;
-    g2.draw(new Arc2D.Double(x-80,y-20,80,80,-30,60,Arc2D.OPEN));
-    x+=Screen.interval*2;
-    y+=Screen.interval*2;
-    nor2(g,x,y);
+}
+
+class Input extends Gate{
+  Input(Point point,boolean truthValue){
+    super(point);
+    outputs_e.add(new Point(70,10));
+    this.name="input";
+    this.truthValue=truthValue;
   }
-  public static void xnor3(Graphics g,int x,int y){
+  public void draw(Graphics g){
     Graphics2D g2=(Graphics2D)g;
-    x-=Screen.interval*2;
-    y-=Screen.interval*2;
-    g2.draw(new Arc2D.Double(x-80,y-20,80,80,-30,60,Arc2D.OPEN));
-    x+=Screen.interval*2;
-    y+=Screen.interval*2;
-    nor3(g,x,y);
-  }
-  public static void input(Graphics g,int x,int y,boolean value){
-    Graphics2D g2=(Graphics2D)g;
-    x-=Screen.interval*2;
-    y-=Screen.interval*2;
+    int x=X-Screen.interval*2;
+    int y=Y-Screen.interval*2;
     Path2D.Double p=new Path2D.Double();
     p.moveTo(x+50,y+10);
     p.lineTo(x+40,y);
@@ -164,16 +240,25 @@ class Gate{
     p.lineTo(x+40,y+20);
     p.lineTo(x+50,y+10);
     p.lineTo(x+66,y+10);
-    if (value)
+    if (truthValue)
       g2.fill(p);
     else
       g2.draw(p);
     dot(g,x+70,y+10);
   }
-  public static void output(Graphics g,int x,int y,boolean value){
+}
+
+class Output extends Gate{
+  Output(Point point,boolean truthValue){
+    super(point);
+    inputs_e.add(new Point(-30,10));
+    this.name="output";
+    this.truthValue=truthValue;
+  }
+  public void draw(Graphics g){
     Graphics2D g2=(Graphics2D)g;
-    x-=Screen.interval*2;
-    y-=Screen.interval*2;
+    int x=X-Screen.interval*2;
+    int y=Y-Screen.interval*2;
     Path2D.Double p=new Path2D.Double();
     p.moveTo(x+40,y+20);
     p.lineTo(x+40,y);
@@ -183,17 +268,49 @@ class Gate{
     p.lineTo(x+40,y+20);
     p.moveTo(x-10,y+10);
     p.lineTo(x-26,y+10);
-    if (value)
+    if (truthValue)
       g2.fill(p);
     else
       g2.draw(p);
     dot(g,x-30,y+10);
   }
-  public static void dot(Graphics g,int x,int y){
+}
+
+class Line extends Gate{
+  public Point begin;
+  public Point end;
+  Line(Point point){
+    super(point);
+    this.name="line";
+  }
+  public void setBegin(Point point){
+    begin=point;
+  }
+  public void setEnd(Point point){
+    end=point;
+  }
+  public Point getBegin(){
+    return begin;
+  }
+  public Point getEnd(){
+    return end;
+  }
+  public void draw(Graphics g){
     Graphics2D g2=(Graphics2D)g;
-    g.setColor(Color.WHITE);
-    g.fillOval(x-4,y-4,8,8);
-    g.setColor(Color.BLACK);
-    g.drawOval(x-4,y-4,8,8);
+    if (begin==null){
+      dot(g,X,Y);
+    }else{
+      dot(g,begin.x,begin.y);
+      Path2D.Double p=new Path2D.Double();
+      p.moveTo(begin.x,begin.y);
+      if (end==null){
+	dot(g,X,Y);
+	p.lineTo(X,Y);
+      }else{
+	dot(g,end.x,end.y);
+	p.lineTo(end.x,end.y);
+      }
+      g2.draw(p);
+    }
   }
 }
